771db8a4bdd4891c81585d02b724fe515a7ae777
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-04-27 00:08:10 +0100

    first bar

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors:{
+        'amazon-blue': '#05253c'
+      }
+    },
+  },
+  plugins: [],
+}
+
