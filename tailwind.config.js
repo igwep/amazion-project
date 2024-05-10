c47c08546c774ac793df3ff46ee34a4380edd5c2
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-05-10 15:18:36 +0100

    still resolving

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./*.{html,js}"],
   theme: {
     extend: {
+      screens:{
+        "tablet": "2900px",
+        "smalls": "709px",
+        "another": "900px"
+      },
       colors:{
         'amazon-blue': '#131921'
       }
